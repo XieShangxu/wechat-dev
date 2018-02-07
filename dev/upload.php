447afbfe133a6e936707b9762447cce956f57e32
Danie Xie <daniel.xie@farfetch.com>
2018-02-07 16:00:36 +0800

    add vconsole

--- dev/upload.php
+++ dev/upload.php
@@ -10,6 +10,10 @@ $signPackage = $jssdk->GetSignPackage();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="./vconsole.min.js"></script>
+    <script>
+      var vconsole = new VConsole();
+    </script>
     <title>Upload</title>
     <style>
         * { margin: 0; padding: 0; }
